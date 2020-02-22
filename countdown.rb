992774241a4c524a279127bfa55b5d4fa0b7fe3a
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-22 17:27:18 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
 
-def countdown
+def countdown(counts)
+  while counts > 0
+  puts "#{counts} SECOND(S)!"
+    counts -= 1
+    # countdown_with_sleep(seconds)
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds)
+  sleep(seconds)
 end
